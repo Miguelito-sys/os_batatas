94b7a21709bc08779f47eb0255b9c5b499c3edd7
luquinhasxx <lucaskauanxp@gmail.com>
2024-11-25 23:00:19 -0300

    Create pp.js

---
+++ pp.js
@@ -0,0 +1,49 @@
+const url = 'https://www.instagram.com/eventos_e_cia_master/?next=%2F'
+const porta = document.querySelector('#porta')
+
+function openInNewTab(url) {
+    const nova = window.open(url, '_blank')
+    nova.focus()
+}
+
+porta.addEventListener('click', ()=>{
+    openInNewTab(url)
+});
+
+const url = 'https://www.instagram.com/eventos_e_cia_master/?next=%2F'
+const porta = document.querySelector('#porta')
+
+function openInNewTab(url) {
+    const nova = window.open(url, '_blank')
+    nova.focus()
+}
+
+porta.addEventListener('click', ()=>{
+    openInNewTab(url)
+});
+
+const url = 'https://www.instagram.com/eventos_e_cia_master/?next=%2F'
+const porta = document.querySelector('#porta')
+
+function openInNewTab(url) {
+    const nova = window.open(url, '_blank')
+    nova.focus()
+}
+
+porta.addEventListener('click', ()=>{
+    openInNewTab(url)
+});
+
+
+
+const url = 'file:///C:/Users/lucas/Desktop/prova%20final/casamento.html'
+const sapo = document.querySelector('sapo')
+
+function openInNewTab(url) {
+    const nova = window.open(url, '_blank')
+    nova.focus()
+}
+
+sapo.addEventListener('click', ()=>{
+    openInNewTab(url)
+});
